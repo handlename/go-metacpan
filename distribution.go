c597909a27232d59254a16b9dbd11ae9b3702a72
handlename <nagata@handlena.me>
2014-12-03 08:16:21 +0900

    devide files for each API

---
+++ distribution.go
@@ -0,0 +1,54 @@
+package metacpan
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
+// The Distribution defines type of a distribution.
+type Distribution struct {
+	ID string `json:"_id"`
+}
+
+// Name returns module name.
+func (d Distribution) Name() string {
+	return strings.Replace(d.ID, "-", "::", -1)
+}
+
+// URL returns url on metacpan.
+func (d Distribution) URL() string {
+	return fmt.Sprintf("https://metacpan.org/pod/%s", d.Name())
+}
+
+// The DistributionHits defines search results.
+type DistributionHits struct {
+	Hits []Distribution `json:"hits"`
+}
+
+// The SearchDistributionResult defines result of search distribution.
+type SearchDistributionResult struct {
+	Hits DistributionHits `json:"hits"`
+}
+
+// SearchDistribution search distribution by query and returns hits.
+func SearchDistribution(q string) ([]Distribution, error) {
+	body, err := request(fmt.Sprintf("%s%s?q=%s", host, APISearchDistribution, q))
+
+	fmt.Println(string(body))
+
+	if err != nil {
+		fmt.Println("error at request search distribution.")
+		return nil, err
+	}
+
+	var result SearchDistributionResult
+	err = json.Unmarshal(body, &result)
+
+	if err != nil {
+		fmt.Println("error at unmarshal response of search distribution.")
+		return nil, err
+	}
+
+	return result.Hits.Hits, nil
+}
--- metacpan.go
+++ metacpan.go
@@ -1,11 +1,9 @@
 package metacpan
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"strings"
 )
 
 // API host
@@ -15,53 +13,6 @@ const host = "https://api.metacpan.org"
 const (
 	APISearchDistribution = "/v0/distribution/_search"
 )
-
-// The Distribution defines type of a distribution.
-type Distribution struct {
-	ID string `json:"_id"`
-}
-
-// Name returns module name.
-func (d Distribution) Name() string {
-	return strings.Replace(d.ID, "-", "::", -1)
-}
-
-// URL returns url on metacpan.
-func (d Distribution) URL() string {
-	return fmt.Sprintf("https://metacpan.org/pod/%s", d.Name())
-}
-
-// The DistributionHits defines search results.
-type DistributionHits struct {
-	Hits []Distribution `json:"hits"`
-}
-
-// The SearchDistributionResult defines result of search distribution.
-type SearchDistributionResult struct {
-	Hits DistributionHits `json:"hits"`
-}
-
-// SearchDistribution search distribution by query and returns hits.
-func SearchDistribution(q string) ([]Distribution, error) {
-	body, err := request(fmt.Sprintf("%s%s?q=%s", host, APISearchDistribution, q))
-
-	fmt.Println(string(body))
-
-	if err != nil {
-		fmt.Println("error at request search distribution.")
-		return nil, err
-	}
-
-	var result SearchDistributionResult
-	err = json.Unmarshal(body, &result)
-
-	if err != nil {
-		fmt.Println("error at unmarshal response of search distribution.")
-		return nil, err
-	}
-
-	return result.Hits.Hits, nil
-}
 
 func request(urlStr string) ([]byte, error) {
 	resp, err := http.DefaultClient.Get(urlStr)
